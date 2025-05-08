58693d7c0b50d3900956a4bfdf6b2946e051c7f5
Asril <139680769+AsrilSyahputra@users.noreply.github.com>
2025-05-08 17:53:39 +0700

    Add files via upload

--- style.js
+++ style.js
@@ -1,33 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Select the hamburger button
   const mobileNavToggle = document.querySelector(".mobile-nav-toggle");
-  // Select the navigation menu
+
   const primaryNav = document.querySelector("#primary-navigation");
 
-  // Check if both elements exist in the DOM
   if (mobileNavToggle && primaryNav) {
-    // Add a click event listener to the hamburger button
     mobileNavToggle.addEventListener("click", () => {
-      // Check if the navigation is currently visible by looking for the 'nav-active' class
       const isVisible = primaryNav.classList.contains("nav-active");
 
-      // Toggle the 'nav-active' class on the navigation menu
       primaryNav.classList.toggle("nav-active");
 
-      // Update the 'aria-expanded' attribute on the button for accessibility
       mobileNavToggle.setAttribute("aria-expanded", !isVisible);
 
-      // Optional: Prevent body scrolling when the navigation menu is open
       if (!isVisible) {
-        // If the menu was not visible and is now being opened
         document.body.style.overflow = "hidden";
       } else {
-        // If the menu was visible and is now being closed
-        document.body.style.overflow = ""; // Allow body scrolling again
+        document.body.style.overflow = "";
       }
     });
 
-    // Optional: Close the mobile menu if a link inside it is clicked
     primaryNav.querySelectorAll("a").forEach((link) => {
       link.addEventListener("click", (event) => {
         const isSamePageAnchor =
@@ -42,7 +32,6 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     });
 
-    // Optional: Close the mobile menu when clicking outside of it
     document.addEventListener("click", (event) => {
       if (
         primaryNav.classList.contains("nav-active") &&
